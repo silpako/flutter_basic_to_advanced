e3fe5d2f789a89656be8e55e816dcdbda5385e6e
silpako <silpako167@gmail.com>
2025-11-07 23:13:38 +0530

    asset

--- lib/1_Storages/Sqflite/C_R_U_D/sqflite_contact.dart
+++ lib/1_Storages/Sqflite/C_R_U_D/sqflite_contact.dart
@@ -4,9 +4,7 @@ import 'package:flutter_basic_to_advanced/1_Storages/Sqflite/C_R_U_D/sql_functio
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Contact_Book(),
-  ));
+  runApp(MaterialApp(home: Contact_Book()));
 }
 
 class Contact_Book extends StatefulWidget {
@@ -21,9 +19,7 @@ class _Contact_BookState extends State<Contact_Book> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("MyContacts"),
-      ),
+      appBar: AppBar(title: const Text("MyContacts")),
       body: isLoading
           ? Text(
               "Create New Contact +",
@@ -44,17 +40,19 @@ class _Contact_BookState extends State<Contact_Book> {
                     trailing: Wrap(
                       children: [
                         IconButton(
-                            onPressed: () => showSheet(contacts[index]['id']),
-                            icon: const Icon(Icons.edit)),
+                          onPressed: () => showSheet(contacts[index]['id']),
+                          icon: const Icon(Icons.edit),
+                        ),
                         IconButton(
-                            onPressed: () =>
-                                deleteContact(contacts[index]['id']),
-                            icon: const Icon(Icons.delete))
+                          onPressed: () => deleteContact(contacts[index]['id']),
+                          icon: const Icon(Icons.delete),
+                        ),
                       ],
                     ),
                   ),
                 );
-              }),
+              },
+            ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => showSheet(null),
         child: const Icon(Icons.person_add_alt_1_outlined),
@@ -67,55 +65,61 @@ class _Contact_BookState extends State<Contact_Book> {
 
   void showSheet(int? id) {
     if (id != null) {
-      final existingcontact =
-          contacts.firstWhere((element) => element['id'] == id);
+      final existingcontact = contacts.firstWhere(
+        (element) => element['id'] == id,
+      );
       name_controller.text = existingcontact['cname'];
       num_controller.text = existingcontact['cnumber'];
     }
 
     showModalBottomSheet(
-        isScrollControlled: true,
-        context: context,
-        builder: (context) {
-          return Container(
-            padding: EdgeInsets.only(
-                top: 15,
-                left: 15,
-                right: 15,
-                bottom: MediaQuery.of(context).viewInsets.bottom + 120),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextField(
-                  controller: name_controller,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), hintText: "Name"),
+      isScrollControlled: true,
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: EdgeInsets.only(
+            top: 15,
+            left: 15,
+            right: 15,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 120,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: name_controller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Name",
                 ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: num_controller,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), hintText: "Number"),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: num_controller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Number",
                 ),
-                ElevatedButton(
-                    onPressed: () {
-                      if (id == null) {
-                        createContact(
-                            name_controller.text, num_controller.text);
-                      }
-                      if (id != null) {
-                        updateContact(id);
-                      }
-                      name_controller.text = "";
-                      num_controller.text = "";
-                      Navigator.of(context).pop();
-                    },
-                    child:
-                        Text(id == null ? "Create Contact" : "Update Contact"))
-              ],
-            ),
-          );
-        });
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (id == null) {
+                    createContact(name_controller.text, num_controller.text);
+                  }
+                  if (id != null) {
+                    updateContact(id);
+                  }
+                  name_controller.text = "";
+                  num_controller.text = "";
+                  Navigator.of(context).pop();
+                },
+                child: Text(id == null ? "Create Contact" : "Update Contact"),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   Future<void> createContact(String name, String number) async {
@@ -140,7 +144,10 @@ class _Contact_BookState extends State<Contact_Book> {
 
   Future<void> updateContact(int id) async {
     await SQL_Functions.updateContactt(
-        id, name_controller.text, num_controller.text);
+      id,
+      name_controller.text,
+      num_controller.text,
+    );
     readContact_and_refresh_Ui(); // to update the list after updating contact
   }
 
@@ -155,8 +162,9 @@ class _Contact_BookState extends State<Contact_Book> {
             onPressed: () async {
               await SQL_Functions.removeContact(id);
               readContact_and_refresh_Ui();
-              ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text("SuccessFully Deleted")));
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text("SuccessFully Deleted")));
               Navigator.pop(context);
             },
             child: const Text('Yes'),

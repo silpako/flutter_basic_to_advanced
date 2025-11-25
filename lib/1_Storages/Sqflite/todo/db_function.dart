af822c2aee5c5cb24d4afd183799c00405d6f72f
silpako <silpako167@gmail.com>
2025-11-25 14:50:50 +0530

    sqflite

---
+++ lib/1_Storages/Sqflite/todo/db_function.dart
@@ -0,0 +1,54 @@
+import 'package:sqflite/sqflite.dart' as sql;
+
+class SQLHelper {
+  ///create database
+  static Future<sql.Database> openOrCreateDB() async {
+    return await sql.openDatabase(
+      'todo',
+      version: 1,
+      onCreate: (database, version) async {
+        await createTable(database);
+      },
+    );
+  }
+
+  /// create table
+  static Future<void> createTable(sql.Database database) async {
+    await database.execute(
+      'CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title TEXT, content TEXT)',
+    );
+  }
+
+  static Future<int> create(String title, String content) async {
+    final db = await SQLHelper.openOrCreateDB(); // to open database
+    var data = {'title': title, 'content': content};
+    final id = await db.insert('task', data);
+    return id;
+  }
+
+  static Future<List<Map<String, dynamic>>> readTask() async {
+    final db = await SQLHelper.openOrCreateDB(); // to open database
+    return db.query('task', orderBy: 'id');
+  }
+
+  static Future<int> update(int id, String utitle, String ucontent) async {
+    final db = await SQLHelper.openOrCreateDB(); // to open database
+    var udata = {'title': utitle, 'content': ucontent};
+    final uid = await db.update(
+      'task',
+      udata,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    return uid;
+  }
+
+  static Future<void> deleteTask(int id) async {
+    final db = await SQLHelper.openOrCreateDB(); // to open database
+    try {
+      await db.delete('task', where: 'id = ?', whereArgs: [id]);
+    } catch (e) {
+      throw Exception();
+    }
+  }
+}

a8c44eb5e62dde7eb830e3673ac3094758b9a4bf
silpako <silpako167@gmail.com>
2025-11-26 22:01:53 +0530

    provider

---
+++ lib/State_management/using single provider/controller/counter_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basic_to_advanced/State_management/using%20single%20provider/model/counter.dart';
+
+class CounterProvider extends ChangeNotifier {
+  final Counter _counter = Counter(0); // private ayond getter tazhey koduthath
+  Counter get counter => _counter;
+
+  void increment() {
+    _counter.count++;
+    notifyListeners();
+  }
+
+  void decrement() {
+    _counter.count--;
+    notifyListeners();
+  }
+}
---
+++ lib/State_management/using single provider/screen/counter_consumer.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basic_to_advanced/State_management/using%20single%20provider/controller/counter_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: ChangeNotifierProvider(
+        create: (context) => CounterProvider(),
+        child: IncreCount(),
+      ),
+    ),
+  );
+}
+
+class IncreCount extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: const Text("Counter"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Press Button to Increment value",
+              style: TextStyle(fontSize: 15),
+            ),
+            Consumer<CounterProvider>(
+              builder:
+                  (
+                    BuildContext context,
+                    CounterProvider counterProvider,
+                    Widget? child,
+                  ) {
+                    return Text(
+                      "Count : ${counterProvider.counter.count}",
+                      style: const TextStyle(fontSize: 25),
+                    );
+                  },
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                InkWell(
+                  onTap: () {
+                    Provider.of<CounterProvider>(
+                      context,
+                      listen: false,
+                    ).increment();
+                  },
+                  child: const CircleAvatar(
+                    backgroundColor: Colors.green,
+                    child: Icon(Icons.add),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    Provider.of<CounterProvider>(
+                      context,
+                      listen: false,
+                    ).decrement();
+                  },
+                  child: const CircleAvatar(
+                    backgroundColor: Colors.red,
+                    child: Icon(Icons.minimize_rounded),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
